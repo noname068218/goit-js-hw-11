407a4e429daca7680740bf376597d7a4f95e3fd2
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-10-01 22:41:24 +0200

    end

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,124 @@
-import axios from 'axios';
-import Notiflix from 'notiflix';
-import simpleLightbox from 'simplelightbox';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios, { isCancel, AxiosError } from 'axios';
 
-const refs = {
-  form: document.querySelector('.search-form'),
-  button: document.querySelector('.gallery'),
-};
-
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-const KEY_API = '39688080-559ef29e62f6f96d97ef6163c';
-const image_type = 'photo';
-const orientation = 'horizontal';
-const safesearch = 'true';
-const TOTAL_PAGE = 40;
-let defaulPage;
-
-const simple = async function (params) {
-  const response = await axios.get(`${KEY_API}?${params}`);
+const BASIC_URL = 'https://pixabay.com/api/';
+const URL_KEY = '39188541-a1bd6d68f6e7210f6abdbcfe1';
+const form = document.querySelector('form');
+const { searchInput } = form.elements;
+const gallery = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
+const finalMessage = document.querySelector('.final-message');
+let pageNumber;
+let simpleGallery = null;
+const fetchImgs = async params => {
+  const response = await axios.get(`${BASIC_URL}?${params}`);
   return response.data;
-  console.log(response);
 };
+
+form.addEventListener('submit', handlerFunction);
+loadMoreButton.addEventListener('click', loadMoreFunction);
+
+function handlerFunction(event) {
+  event.preventDefault();
+  pageNumber = 1;
+  const params = new URLSearchParams({
+    key: URL_KEY,
+    q: searchInput.value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 39,
+  });
+
+  fetchImgs(params)
+    .then(imgs => {
+      if (imgs.hits.length === 0) {
+        if (simpleGallery !== null) simpleGallery.refresh();
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+
+      gallery.innerHTML = createMarkUp(imgs.hits);
+      loadMoreButton.removeAttribute('hidden');
+      finalMessage.setAttribute('hidden', 'true');
+      simpleGallery = new SimpleLightbox('.gallery a', {
+        /* options */
+      });
+      Notify.success(`Hooray! We found ${imgs.totalHits} images.`);
+      if (40 > imgs.totalHits) {
+        loadMoreButton.setAttribute('hidden', 'true');
+        return finalMessage.removeAttribute('hidden');
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function loadMoreFunction() {
+  pageNumber += 1;
+  const params = new URLSearchParams({
+    key: URL_KEY,
+    q: searchInput.value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: pageNumber,
+    per_page: 39,
+  });
+
+  fetchImgs(params).then(imgs => {
+    if (pageNumber - 1 > imgs.totalHits / 39) {
+      loadMoreButton.setAttribute('hidden', 'true');
+      return finalMessage.removeAttribute('hidden');
+    }
+
+    gallery.insertAdjacentHTML('beforeend', createMarkUp(imgs.hits));
+    setTimeout(() => {
+      simpleGallery.refresh();
+    }, 50);
+  });
+}
+
+export function createMarkUp(imgs) {
+  const markUp = imgs
+    .map(
+      ({
+        downloads,
+        comments,
+        views,
+        tags,
+        webformatURL,
+        largeImageURL,
+        likes,
+      }) => {
+        return `<div class="photo-card">
+        <a href= "${largeImageURL}"><img src="${webformatURL}" class="photo" width="600px" height="400px" alt="${tags}" loading="lazy" /></a>
+            <div class="info">
+            <p class="info-item">
+                <b>Likes</b>
+                ${likes}
+                </p>
+            <p class="info-item">
+                <b>Views</b>
+                ${views}
+                </p>
+            <p class="info-item">
+                <b>Comments</b>
+                ${comments}
+                </p>
+            <p class="info-item">
+                <b>Downloads</b>
+                ${downloads}
+                </p>
+            </div>
+    </div>`;
+      }
+    )
+    .join('');
+
+  return markUp;
+}
